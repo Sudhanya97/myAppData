96d26ab1b407d332323bc191125252b3710cba2a
Sudhanya Mukhopadhyay <sudhanya.mukhopadhyay@gmail.com>
2019-09-28 15:13:09 -0700

    setter and getter added

--- book.java
+++ book.java
@@ -1,5 +1,25 @@
 public Class book
 {
 	private String name;
+	private String city;
+}
 
+public void setName(String name)
+{
+	this.name = name;
+}
+
+public void setCity(String city)
+{
+	this.city =  city;
+}
+
+public void getName()
+{
+	return name;
+}
+
+public void getCity()
+{
+	return city;
 }
